6704850d1c30bf7d3f90875cf36740520c5c59ce
keith <keith@raincityambience.com>
2018-07-26 21:26:34 -0700

    add datalist / tabs showing json data via listview

--- src/components/NativeCourses.js
+++ src/components/NativeCourses.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, ListView, Image } from 'react-native';
+import data from '../data/courses.json';
+
+const ds = new ListView.DataSource({
+  rowHasChanged: (r1, r2) => r1 !== r2
+});
+const dataSource = ds.cloneWithRows(data);
 
 class NativeCourses extends Component {
 
@@ -11,11 +17,30 @@ class NativeCourses extends Component {
     const {navigate} = this.props.navigation;
     return (
       <View>
-        <Text>React Native Courses</Text>
         <Button
           title='React  Courses'
-          onPress={() => navigate('ReactCourses')}
-          />
+          onPress={() => navigate('ReactCourses')}/>
+
+        <ListView
+          dataSource={dataSource}
+          renderRow={(rowData) =>
+            <View>
+              <Text>{rowData.title}</Text>
+              <Text>{rowData.description}</Text>
+              <Text>{rowData.view}</Text>
+              <Button
+                onPress={()=>{console.log('pressedin rncourses')}}
+                title="Link to course" />
+              <Text>{rowData.link}</Text>
+              <Image
+                source={{uri: rowData.image}}
+                style={{width: 400, height: 200}} />
+
+            </View>
+          }
+        />
+
+
       </View>
     )
   }
--- src/components/ReactCourses.js
+++ src/components/ReactCourses.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, ListView, Image } from 'react-native';
+import data from '../data/courses.json';
+
+const ds = new ListView.DataSource({
+  rowHasChanged: (r1, r2) => r1 !== r2
+});
+const dataSource = ds.cloneWithRows(data);
 
 class ReactCourses extends Component {
 
@@ -10,15 +16,39 @@ class ReactCourses extends Component {
   render(){
     const {navigate} = this.props.navigation;
     return (
-      <View>
-        <Text>React Courses</Text>
+      <View style={styles.container}>
         <Button
           title='React Native Courses'
-          onPress={() => navigate('NativeCourses')}
-          />
+          onPress={() => navigate('NativeCourses')} />
+
+        <ListView
+          dataSource={dataSource}
+          renderRow={(rowData) =>
+            <View>
+              <Text>{rowData.title}</Text>
+              <Text>{rowData.description}</Text>
+              <Text>{rowData.views}</Text>
+              <Button
+                onPress={()=>console.log('pressed')}
+                title="Link to course"/>
+              <Text>{rowData.link}</Text>
+              <Image
+                source={{uri: rowData.image}}
+                style={{width: 400, height: 200}} />
+            </View>
+          }
+         />
       </View>
     )
   }
 }
 
+
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+    paddingTop: 10
+  }
+}
 export default ReactCourses;
